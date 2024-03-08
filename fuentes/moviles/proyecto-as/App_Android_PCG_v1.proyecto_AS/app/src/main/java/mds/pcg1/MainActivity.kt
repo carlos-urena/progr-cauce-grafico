2587ce5c4661a5ef120aada1c00c7a879b36bec8
carlos-urena <curena@ugr.es>
2024-03-08 23:45:02 +0100

    trabajando en vec_mat.kt

--- fuentes/moviles/proyecto-as/App_Android_PCG_v1.proyecto_AS/app/src/main/java/mds/pcg1/MainActivity.kt
+++ fuentes/moviles/proyecto-as/App_Android_PCG_v1.proyecto_AS/app/src/main/java/mds/pcg1/MainActivity.kt
@@ -1,3 +1,27 @@
+// *********************************************************************
+// **
+// ** Asignatura: PCG (Programación del Cauce Gráfico).
+// **
+// ** Implementación de la actividad principal
+// ** Copyright (C) 2024 Carlos Ureña
+// **
+// ** Clase: MainActivity
+// **
+// ** This program is free software: you can redistribute it and/or modify
+// ** it under the terms of the GNU General Public License as published by
+// ** the Free Software Foundation, either version 3 of the License, or
+// ** (at your option) any later version.
+// **
+// ** This program is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// ** GNU General Public License for more details.
+// **
+// ** You should have received a copy of the GNU General Public License
+// ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// **
+// *********************************************************************
+
 package mds.pcg1
 
 import android.os.Bundle
@@ -13,6 +37,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import mds.pcg1.ui.theme.AppAndroidPCGV1Theme
 
 import android.util.Log
+
+import mds.pcg1.utilidades.*
+import mds.pcg1.vec_mat.*
+
+
 
 class MainActivity : ComponentActivity()
 {
@@ -31,7 +60,10 @@ class MainActivity : ComponentActivity()
                 }
             }
         }
-        Log.v( TAG, "Inicialización de la aplicación finalizada")
+        Log.v( TAG, "Inicialización de la aplicación finalizada - va test")
+
+        var test = VecMatTest() ;
+        test.run()
     }
 }
 // ----------------------------------------------------------------------------
--- fuentes/moviles/proyecto-as/App_Android_PCG_v1.proyecto_AS/app/src/main/java/mds/pcg1/utilidades.kt
+++ fuentes/moviles/proyecto-as/App_Android_PCG_v1.proyecto_AS/app/src/main/java/mds/pcg1/utilidades.kt
@@ -1,13 +1,13 @@
-package mds.pcg1
-
+package mds.pcg1.utilidades
 
 /**
  * Etiqueta para mensajes impresos con Log.v
  * Ver: [esto](https://stackoverflow.com/questions/45841067/what-is-the-best-way-to-define-log-tag-constant-in-kotlin))
  * @return 'CUA +  nombre de la clase'
  */
-val Any.TAG: String
-    get() {
+val Any.TAG : String get()
+    {
         val tag = javaClass.simpleName
         return "CUA $tag"
     }
+
